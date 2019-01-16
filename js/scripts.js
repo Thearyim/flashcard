ed0ce6174ffbdd765959890bfc2686818dc2faa7
Porter Savage and Theary Im <theary@>
2019-01-15 16:28:51 -0800

    add flascard project

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function) {
+  $(".panel").click(function() {
+    $("#flashcard-hidden").slideToggle();
+    $("#flashcard-showing").slideToggle();
+  });
+
+  $(".panel1").click(function() {
+    $("#flashcard-hidden").slideToggle();
+    $("#flashcard-showing").slideToggle();
+  });
+});
